85e1567be720bad0ba798e4616ec64e8fd321418
Kevin Thumbar <kevthummar178@gmail.com>
2024-07-24 18:59:53 +0530

    resolved the api config error

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,13 +1,16 @@
-const express = require('express')
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
+import { userRouter } from './routes/user';
+import { zapRouter } from './routes/zap';
+
 const app = express();
 
 const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-app.use('api/v1/user, userRouter');
-app.use('api/v1/zap, joltRouter');
+app.use('api/v1/user', userRouter);
+app.use('api/v1/zap', zapRouter);
 
 
 app.listen(port, () => {
