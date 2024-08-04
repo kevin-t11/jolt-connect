90f57c0303860197637f2cbac308e4c12a4db85f
Kevin Thumbar <kevthummar178@gmail.com>
2024-08-04 23:37:09 +0530

    tested the all end point and modifed

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,18 +1,15 @@
 import express from 'express';
-import cors from 'cors';
 import { userRouter } from './routes/user';
 import { zapRouter } from './routes/zap';
 
 const app = express();
 
-const port = process.env.PORT || 3000;
-app.use(express.json());
-app.use(cors());
+app.use(express.json()); 
 
-app.use('api/v1/user', userRouter);
-app.use('api/v1/zap', zapRouter);
+app.use('/api/v1/user', userRouter);
+app.use('/api/v1/zap', zapRouter);
 
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
--- apps/api/src/routes/user.ts
+++ apps/api/src/routes/user.ts
@@ -1,5 +1,4 @@
-
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import { authMiddleware } from "../middleware";
 import db from "@joltconnect/db";
 import jwt from "jsonwebtoken";
@@ -8,67 +7,66 @@ import { SigninSchema, SignupSchema } from "../types";
 
 const router = Router();
 
-router.post("/signup", async (req, res) => {
+router.post("/signup", async (req: Request, res: Response) => {
+
     const body = req.body;
     const parsedData = SignupSchema.safeParse(body);
 
     if (!parsedData.success) {
-        console.log(parsedData.error);
         return res.status(411).json({
             message: "Incorrect inputs"
-        })
+        });
     }
 
     const userExists = await db.user.findFirst({
         where: {
-            email: parsedData.data.username
+            email: parsedData.data.email
         }
     });
 
     if (userExists) {
         return res.status(403).json({
             message: "User already exists"
-        })
+        });
     }
 
     await db.user.create({
         data: {
-            email: parsedData.data.username,
-            // TODO: Dont store passwords in plaintext, hash it
+            email: parsedData.data.email,
+            // TODO: Don't store passwords in plaintext, hash it
             password: parsedData.data.password,
             name: parsedData.data.name
         }
-    })
+    });
 
     // await sendEmail();
 
     return res.json({
         message: "Please verify your account by checking your email"
     });
+});
 
-})
-
-router.post("/signin", async (req, res) => {
+router.post("/signin", async (req: Request, res: Response) => {
     const body = req.body;
     const parsedData = SigninSchema.safeParse(body);
 
     if (!parsedData.success) {
         return res.status(411).json({
             message: "Incorrect inputs"
-        })
+        });
     }
 
     const user = await db.user.findFirst({
         where: {
-            email: parsedData.data.username,
+            email: parsedData.data.email,
             password: parsedData.data.password
         }
     });
-    
+
     if (!user) {
         return res.status(403).json({
             message: "Sorry credentials are incorrect"
-        })
+        });
     }
 
     // sign the jwt
@@ -79,9 +77,9 @@ router.post("/signin", async (req, res) => {
     res.json({
         token: token,
     });
-})
+});
 
-router.get("/", authMiddleware, async (req, res) => {
+router.get("/", authMiddleware, async (req: Request, res: Response) => {
     // TODO: Fix the type
     // @ts-ignore
     const id = req.id;
@@ -98,6 +96,6 @@ router.get("/", authMiddleware, async (req, res) => {
     return res.json({
         user
     });
-})
+});
 
 export const userRouter = router;
